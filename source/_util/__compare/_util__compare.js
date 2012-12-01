97d72fce7a6a340015d9add9b3dae3789cf4b6cc
Alexander Tarmolov <tarmolov@gmail.com>
2012-12-01 18:41:45 +0400

    deep compare util

---
+++ source/_util/__compare/_util__compare.js
@@ -0,0 +1,86 @@
+sm.plugin('_util.compare', function (sandbox) {
+
+    var toString = Object.prototype.toString;
+
+    function isObject (obj) {
+        return toString.apply(obj) === '[object Object]';
+    }
+
+    function compareObjects (a, b) {
+        // Compare if they are references to each other
+        if (a === b) {
+            return true;
+        }
+
+        if (Object.keys(a).length !== Object.keys(b).length) {
+            return false;
+        }
+
+        for (var i in a) {
+            if (
+                a.hasOwnProperty(i) && (
+                    typeof b[i] === 'undefined' ||
+                    toString.apply(objA[i]) !== toString.apply(objB[i])
+                ) ||
+                !compare(objA[i],objB[i])
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function isArray (arr) {
+        return toString.apply(arr) === '[object Array]';
+    }
+
+    function compareArrays(a, b) {
+        // References to each other?
+        if (a === b) {
+            return true;
+        }
+
+        // Arrays with different length are not equal
+        if (a.length !== b.length) {
+            return false;
+        }
+
+        for (var i = 0, l = a.length; i < l; i+=1) {
+            if (toString.apply(a[i]) !== toString.apply(b[i])) {
+                return false;
+            }
+
+            if (!compare(a[i],b[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function isFloat(n) {
+        return typeof n === 'number' && n % 1 !== 0;
+    }
+
+    function compareFloats(a, b) {
+        return a.toFixed(6) ==  b.toFixed(6);
+    }
+
+    function compare (a, b) {
+        switch (Boolean(true)) {
+            case toString.apply(a) !== toString.apply(b):
+                return false;
+            case isObject(a):
+                return compareObjects(a, b);
+            case isArray(a):
+                return compareArrays(a, b);
+            case isFloat(a):
+                return compareFloats(a, b);
+            default:
+                return a === b;
+        }
+    }
+
+    return compare;
+
+});
